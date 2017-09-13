3f752ae22aa63a34a93c7f1e13b03dea4889629a
Austin <elektricwebdesign@gmail.com>
2017-09-12 20:08:33 -0600

    Packages added to client folder: * "react-router": "^4.2.0" * "redux": "^3.7.2" * "redux-observable": "^0.16.0" * "rxjs": "^5.4.3"
    Packages added to server:
    * "express-session": "^1.15.5"
    * "mongoose": "^4.11.11"
    * "passport-github": "^1.1.0"
    
    client is not wired up to the initial Store.
    TODO: configure React-Router to work with the store and begin inital
    routing system

---
+++ client/reducers/index.js
@@ -0,0 +1,11 @@
+
+/**
+  rootReducer
+*/
+
+import { combineReducers } from 'redux';
+import home from './homeReducer';
+
+export default combineReducers({
+  home,
+});
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const config = require('../webpack.dev.config.js');
-
+const homeRouter = require('./src/routes/home');
 const app = express();
 const compiler = webpack(config);
 
@@ -11,7 +11,7 @@ const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
 }));
-
+app.get('/', homeRouter);
 // Serve the files on port 3000.
 app.listen(3000, () => {
     console.info('Example app listening on port 3000!\n');
