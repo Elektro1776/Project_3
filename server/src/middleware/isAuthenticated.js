2a7c968d67a10964de389ef377cd79eda9da74ff
Austin <elektricwebdesign@gmail.com>
2017-10-04 17:48:55 -0600

    WORK IN PROGRESSS ON ROUTING AND AUTH SHIT

--- server/src/middleware/isAuthenticated.js
+++ server/src/middleware/isAuthenticated.js
@@ -1,13 +1,13 @@
 let inc = 0;
-export const isAuthenticated = (req, res, next) => {
-  inc += 1;
-  console.info(' WHAT ARE THE KEYS ON SESSION???', req.path);
-  // console.info("Are we authenticated?", req.session);
-  if (req.session || req.path === '/about') {
-    console.log(' WHAT IS THE REQ.SESSION.USER', req.session);
-    res.redirect('/about');
-  } else {
-    return res.redirect('/about');
-  }
-  // next()
-};
+// export const isAuthenticated = (req, res, next) => {
+//   inc += 1;
+//   console.info(' WHAT ARE THE KEYS ON SESSION???', req.path);
+//   // console.info("Are we authenticated?", req.session);
+//   if (req.session || req.path === '/about') {
+//     console.log(' WHAT IS THE REQ.SESSION.USER', req.session);
+//     res.redirect('/about');
+//   } else {
+//     return res.redirect('/about');
+//   }
+//   // next()
+// };
