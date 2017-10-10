8c0c39f84e9a7b4fa6bcd9c684fdad7333b45b01
Austin <elektricwebdesign@gmail.com>
2017-10-10 02:27:24 -0600

    Initial jwt stuff put into place , cleaned up console.logs to console.infos to stop linter whining lulzzz….. Also added an initial action to send off the first request to verify a user. TODO : implement logic to grab token from local storage if its there and dispatch our fancy new action… ADDED PACKAGES: react-hot-loader NEEDS: Testing the token verify and original sign in functions on server side.

---
+++ client/actions/signupActions/index.js
@@ -0,0 +1,46 @@
+export const signupUser = (userData) => (dispatch) => {
+  dispatch(singingUserIn());
+  return fetch('/signup', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response;
+    })
+    .then((result) => result.json())
+    .then((user) => {
+      console.info(' WHAT IS OUR USER ????', user.body);
+      dispatch(successUserSignIn(user));
+    })
+    .catch((err) => {
+      // console.log(' Do we hit an errr::???', err);
+      dispatch(failureUserSignIn(err));
+    });
+};
+// };
+const singingUserIn = () => {
+  console.info(' SIGNING IN USER !');
+  return {
+    type: 'SIGNING_IN_USER',
+  };
+};
+
+const successUserSignIn = (user) => {
+  console.info('Success signed user in', user);
+  return {
+    type: 'SUCCESS_SIGNED_USER_IN',
+  };
+};
+
+const failureUserSignIn = (err) => {
+  console.info(' FAILED TO SIGN USER UP', err);
+  return {
+    type: 'FAILURE_SIGNED_USER_IN',
+  };
+};
