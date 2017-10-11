ed63889de62145c785235ba997b8b4b4e6d68f03
Chelsea Mitchell <kguitar6@Ks-MacBook-Air.local>
2017-10-11 14:51:20 -0600

    finalized styling -- working on logo

--- client/containers/login/index.js
+++ client/containers/login/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import LoginForm from '../../components/LoginForm';
+import styles from '../../components/LoginForm/LoginForm.css'
+// import image from '../../assets/utile.png'
 
 class Login extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 pull-right">
+
+
+      <div className={styles.wrapper}>
+
+
+         <img src={require("../../components/LoginForm/utile.png")} className={styles.img}/>
+
+        <div className={styles.box}>
           <LoginForm />
         </div>
+
       </div>
     );
   }
