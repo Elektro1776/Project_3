d37b453b178ac7e51d36dc5f73b734e69866a773
901david <vdavidhammond@gmail.com>
2017-10-16 10:34:40 -0600

    Made project layout window full screen, added in collaborator bar, re-styled chips to match other colors, stuff shaping up!

--- client/containers/projects/index.js
+++ client/containers/projects/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ProjLayout from './Layout';
 import ButtonBar from '../../components/ButtonBar/Button_Bar';
 import styles from './project_style.css';
-
+import CollaboratorsBar from '../../components/Collaborators';
 
 class Projects extends Component {
     state = {
@@ -29,6 +29,9 @@ class Projects extends Component {
     render() {
       return (
         <div>
+          <div>
+            <CollaboratorsBar />
+          </div>
           <div className={styles.buttonBox}>
             <ButtonBar clicker={this.whatStateToChange.bind(this)} />
           </div>
