cb19cd33d45d4eb0263de07248c55cffd188f714
Chelsea Mitchell <kguitar6@Ks-MacBook-Air.local>
2017-10-16 16:14:43 -0600

    added and styled alt footer + nav

---
+++ client/components/AltFooter/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Button } from 'react-toolbox/lib/button';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class AltFooter extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  
+  }
+
+
+  render() {
+    const bootStrap = 'col-xs-12';
+    const styles = {
+      basic: {
+        width: '100vw',
+        background: '#545454',
+        height: '50px',
+        position: 'fixed',
+        bottom: '0',
+      },
+      icons: {
+        height: '10px',
+        fontSize: 12,
+        color: '#ddd',
+        margin: 'auto',
+        padding: '5px',
+
+      },
+    };
+
+    return (
+      <div style={styles.basic}>
+
+        <Button icon="copyright" label="copyright 2017" className={bootStrap} style={styles.icons} />
+
+      </div>
+    );
+  }
+}
+
+
+
+export default AltFooter;
