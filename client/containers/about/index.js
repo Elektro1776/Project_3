bffc3fc57c54794a1acd8fd9659518cf00dfea03
Chelsea Mitchell <kguitar6@Ks-MacBook-Air.local>
2017-10-16 20:37:32 -0600

    updated styling -- started on about

--- client/containers/about/index.js
+++ client/containers/about/index.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import styles from "../about/about.css";
+
+
 class About extends Component {
+
   render() {
-    return (<div>
-      <p style={{color: 'black'}}>About !!!</p>
-    </div>)
+    return (
+
+      <div className="container">
+
+        <div className="col-lg-3"></div>
+
+        <div className="col-lg-6">
+
+          <h3> About uTile </h3>
+
+          <hr></hr>
+
+          <p>Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor
+        hodor hodor! Hodor, hodor hodor... Hodor hodor hodor;
+        hodor hodor hodor! Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor,
+        hodor hodor... Hodor hodor hodor? Hodor. Hodor hodor, hodor. Hodor
+        hodor hodor hodor! </p>
+
+        </div>
+
+
+      </div>
+    );
   }
 }
+
 export default About;
