7de5e8577eb2b8a627209aa28a8d725a4c207957
901david <vdavidhammond@gmail.com>
2017-10-19 18:10:12 -0600

    added in loader image when projects arent loading

--- client/containers/projects/index.js
+++ client/containers/projects/index.js
@@ -3,9 +3,7 @@ import ProjLayout from './Layout';
 import ButtonBar from '../../components/ButtonBar/Button_Bar';
 import styles from './project_style.css';
 import CollaboratorsBar from '../../components/Collaborators';
-const repoName = 'funRepoCreatedByPostMan';
 import { connect } from 'react-redux';
-const currentUser = '901david';
 
 class Projects extends Component {
     state = {
@@ -14,33 +12,30 @@ class Projects extends Component {
       matrixButt: false,
       codeButt: false,
       currentProject: null,
+      currentScreen: 'readmeButt',
     };
     componentWillReceiveProps(nextProps) {
       // console.log("this should show projects connected in state", nextProps.currentProject);
       const { currentProject } = nextProps;
-      // console.log(' WHAT IS USER CURRENT PROJECT', currentProject);
-      if (currentProject.length !== 0) {
+      if (currentProject.length !== 0 && currentProject.id !== this.props.currentProject.id) {
         this.setState({ currentProject });
       }
     }
     whatStateToChange(prop) {
-      const keyArray = Object.keys(this.state);
-      this.setState({
-        issuesButt: false,
-        readmeButt: false,
-        matrixButt: false,
-        codeButt: false
-      });
-      keyArray.map((key) => {
-        if (key === prop) {
-          this.setState({ [prop]: true });
-        }
-      });
+      this.setState({ currentScreen: prop });
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+      if (nextState.currentProject !== this.state.currentProject) {
+        return true;
+      }
+      if (nextState.currentScreen !== this.state.currentScreen) {
+        return true;
+      }
+      return false;
     }
     render() {
       const { currentProject } = this.state;
       if (currentProject !== null) {
-        // console.log('here is current proj in IF statement', currentProject);
         return (
           <div>
             <div>
@@ -50,17 +45,19 @@ class Projects extends Component {
               <ButtonBar clicker={this.whatStateToChange.bind(this)} />
             </div>
             <div>
-              <ProjLayout state={this.state} repoName={currentProject.name} currentUser={currentProject.owner.login} />
+              <ProjLayout currentScreen={this.state.currentScreen} repoName={currentProject.name} currentUser={currentProject.owner.login} />
             </div>
           </div>
         );
       }
       return (
-        <div>Loading....</div>
-      )
+        <div className={styles.loaderDiv}>
+          <img src='https://raw.githubusercontent.com/Elektro1776/Project_3/development/client/components/Card/uTile_black_loader_50.gif' alt='loader' className={styles.loaderImage} />
+        </div>
+      );
     }
 }
 
-export default connect((state)=>({
+export default connect((state) => ({
   currentProject: state.repos.currentProject,
 }), null)(Projects);
