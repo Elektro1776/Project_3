287bb7e218d5e87c0a9f5e81829e071ce1586fe3
901david <vdavidhammond@gmail.com>
2017-11-04 05:03:32 -0600

    modal has dropdown and generating proper text, now to add in form

--- client/components/Modal/newIssuePull_Modal.js
+++ client/components/Modal/newIssuePull_Modal.js
@@ -1,30 +1,68 @@
 import React, { Component } from 'react';
 import { ModalContainer, ModalDialog } from 'react-modal-dialog';
+import Dropdown from 'react-toolbox/lib/dropdown';
+
+const values = [
+  {value: 'issue', label: 'New Issue'},
+  {value: 'pull_request', label: 'New Pull Request'},
+]
 
 class IssuePullModal extends Component {
   state = {
     showing: 'issue',
   }
-  handlePullRequestDisplay = (type) => {
-    this.setState({ showing: type });
+  handlePullRequestDisplay = (value) => {
+    this.setState({ showing: value });
   }
   render() {
-    // console.log(this.props.value, 'my state');
-    return (
-      <div onClick={this.props.handleIssuePullClick}>
-        {
-          this.props.isShowing &&
-          <ModalContainer onClose={this.props.handleIssuePullClose}>
-            <ModalDialog onClose={this.props.handleIssuePullClose}>
-              <p>Hellow WOrld</p>
-              <button className="btn btn-lg btn-success">Submit</button>
-              <button className="btn btn-lg btn-danger" onClick={this.props.handleIssuePullClose}> Cancel</button>
-            </ModalDialog>
+    if (this.state.showing === 'issue') {
+      return (
+        <div onClick={this.props.handleIssuePullClick}>
+          {
+            this.props.isShowing &&
+            <ModalContainer onClose={this.props.handleIssuePullClose}>
+              <ModalDialog onClose={this.props.handleIssuePullClose}>
+                <Dropdown
+                  auto
+                  label={`Choose Type`}
+                  onChange={this.handlePullRequestDisplay}
+                  source={values}
+                  value={this.state.showing}
+                />
+                <p>New Issue</p>
+                <button className="btn btn-lg btn-success">Submit</button>
+                <button className="btn btn-lg btn-danger" onClick={this.props.handleIssuePullClose}> Cancel</button>
+              </ModalDialog>
 
-          </ModalContainer>
-        }
-      </div>
-    );
+            </ModalContainer>
+          }
+        </div>
+      );
+    }
+    else if (this.state.showing === 'pull_request') {
+      return (
+        <div onClick={this.props.handleIssuePullClick}>
+          {
+            this.props.isShowing &&
+            <ModalContainer onClose={this.props.handleIssuePullClose}>
+              <ModalDialog onClose={this.props.handleIssuePullClose}>
+                <Dropdown
+                  auto
+                  label={`Choose Type`}
+                  onChange={this.handlePullRequestDisplay}
+                  source={values}
+                  value={this.state.showing}
+                />
+                <p>New Pull</p>
+                <button className="btn btn-lg btn-success">Submit</button>
+                <button className="btn btn-lg btn-danger" onClick={this.props.handleIssuePullClose}> Cancel</button>
+              </ModalDialog>
+
+            </ModalContainer>
+          }
+        </div>
+      );
+    }
   }
 }
 export default IssuePullModal;
